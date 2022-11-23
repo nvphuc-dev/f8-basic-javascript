c17707e9b2e9627ea19757f3f98a3f4dbdb0d603
CailyPhuc <phuc_web@caily.com.vn>
2022-11-23 08:14:55 +0700

    Format code

--- resource/js/utilities.js
+++ resource/js/utilities.js
@@ -1,23 +1,23 @@
 /*
 $(document).ready(function(){
-    // jQuery methods go here...
+	// jQuery methods go here...
 });
 */
 
 // Short method
 $(function(){
-    // jQuery Accordion
-    $('.js-accordion .js-accordion-item:nth-child(1) .js-accordion-trigger').addClass('active');
-    $('.js-accordion .js-accordion-item:nth-child(1) .js-accordion-content').slideDown();
-    $('.js-accordion-trigger').on('click', function() {
-        if($(this).hasClass('active')) {
-            $(this).siblings('.js-accordion-content').slideUp();
-            $(this).removeClass('active');
-        } else {
-            $('.js-accordion-content').slideUp();
-            $('.js-accordion-trigger').removeClass('active');
-            $(this).siblings('.js-accordion-content').slideToggle();
-            $(this).toggleClass('active');
-        }
-    });
+	// jQuery Accordion
+	$('.js-accordion .js-accordion-item:nth-child(1) .js-accordion-trigger').addClass('active');
+	$('.js-accordion .js-accordion-item:nth-child(1) .js-accordion-content').slideDown();
+	$('.js-accordion-trigger').on('click', function() {
+		if($(this).hasClass('active')) {
+			$(this).siblings('.js-accordion-content').slideUp();
+			$(this).removeClass('active');
+		} else {
+			$('.js-accordion-content').slideUp();
+			$('.js-accordion-trigger').removeClass('active');
+			$(this).siblings('.js-accordion-content').slideToggle();
+			$(this).toggleClass('active');
+		}
+	});
 });
