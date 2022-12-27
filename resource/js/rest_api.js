8457b11c386b4045fac2375373788f678df6f48b
CailyPhuc <phuc_web@caily.com.vn>
2022-12-27 18:13:59 +0700

    Update progress

--- resource/js/rest_api.js
+++ resource/js/rest_api.js
@@ -5,6 +5,7 @@ function start(){
 	// 	renderCourses(courses);
 	// });
 	getCourses(renderCourses);
+	handleCreateForm();
 }
 
 start();
@@ -17,14 +18,64 @@ function getCourses(callback){
 	}).then(callback);
 }
 
+function storeCourse(data, callback){
+	let options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	};
+
+	fetch(courseApi, options).then(function(response){
+		response.json();
+	}).then(callback);
+}
+
+function handleDeleteCourse(id){
+	let options = {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	fetch(courseApi + '/' + id, options).then(function(response){
+		response.json();
+	}).then(function(){
+		let courseItem = document.querySelector('.lst-courses-id-' + id);
+		if(courseItem){
+			courseItem.remove();
+		}
+	});
+}
+
+function handleCreateForm(){
+	let btn = document.querySelector('#create');
+	btn.onclick = function(){
+		let title = document.querySelector('input[name="title"]').value;
+		let description = document.querySelector('textarea[name="description"]').value;
+		let formData = {
+			title: title,
+			description: description
+		}
+		storeCourse(formData, function(){
+			getCourses(renderCourses);
+		});
+	}
+}
+
 function renderCourses(courses){
 	let listCourses = document.querySelector('#lst-courses');
 	let htmls = courses.map(function(course){
 		return `
-			<li>
+			<li class="lst-courses-id-${course.id}">
 				<div class="lst-courses__inner">
 					<h4>${course.title}</h4>
 					<p>${course.description}</p>
+					<p class="lst-courses__actions">
+						<span class="btn btn--sm btn--danger" onclick=handleDeleteCourse(${course.id})>Xóa</span>
+					</p>
 				</div>
 			</li>
 		`;
